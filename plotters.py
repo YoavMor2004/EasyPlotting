0e9c1d633091fed947193044c46f12bba81671b7
YoavMor2004 <yoavdor9@gmail.com>
2023-12-05 13:10:56 +0200

    added plotter for dashed lines

--- plotters.py
+++ plotters.py
@@ -12,3 +12,7 @@ def big_scatter(ax: plt.Axes, ch_x: np.ndarray, ch_y: np.ndarray, label: str | N
 
 def line_plot(ax: plt.Axes, ch_x: np.ndarray, ch_y: np.ndarray, label: str | None) -> None:
     ax.plot(ch_x, ch_y, label=label)
+
+
+def dashed_line_plot(ax: plt.Axes, ch_x: np.ndarray, ch_y: np.ndarray, label: str | None) -> None:
+    ax.plot(ch_x, ch_y, ls='--', label=label)
