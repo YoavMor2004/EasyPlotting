89aca2f753c9e2f271977dddd108c4888284b43e
YoavMor2004 <yoavdor9@gmail.com>
2023-12-05 21:43:56 +0200

    internal file restructering

--- AxesPlot.py
+++
@@ -1,9 +0,0 @@
-from EasyPlotting.Plot import Plot
-
-
-class AxesPlot(Plot):
-    def __init__(self, title: str, xlabel: str, ylabel: str, dir_path: str | None):
-        super().__init__(title, xlabel, ylabel, dir_path)
-
-        self.ax.axvline(0, color='k')
-        self.ax.axhline(0, color='k')
--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from EasyPlotting.AxesPlot import *
-from EasyPlotting.Plot import *
+from EasyPlotting.Plot.AxesPlot import *
+from EasyPlotting.Plot.Plot import *
 from EasyPlotting.PlotFactory import *
 from EasyPlotting import plotters
